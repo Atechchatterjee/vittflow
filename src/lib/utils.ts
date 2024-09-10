a774d85afc2903ec24303a10401b3864f500e634
Atechchatterjee <atechchatterjeee@gmail.com>
2024-09-10 22:09:52 +0530

    feat: add safeAwait and parseDateTime functionality

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,27 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function parseDateTime(datetime: string): [string, string] {
+  let date: string = datetime.substring(0, 10);
+  const [yy, mm, dd] = date.split("-");
+  date = [dd, mm, yy].join("/");
+  const time: string = datetime.substring(11, 19);
+  return [date, time];
+}
+
+// returns errors as values
+export async function safeAwait<T, E = Error>(
+  promise: Promise<T>,
+): Promise<[E, null] | [null, T]> {
+  try {
+    const res = await promise;
+    return [null, res];
+  } catch (err) {
+    console.error(err);
+    return [err as E, null];
+  }
 }
