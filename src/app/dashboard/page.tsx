7422083530a6ec44faa3118c4ad23d9504aca75f
Atechchatterjee <atechchatterjeee@gmail.com>
2024-09-10 22:12:59 +0530

    feat: add a basic dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,14 +1,24 @@
 import React from "react";
 import AdminSidebar from "~/components/AdminSidebar";
 import { SignedIn, UserButton } from "@clerk/nextjs";
+import { currentUser } from "@clerk/nextjs/server";
 
-export default function Dashboard() {
+export default async function Dashboard() {
+  const user = await currentUser();
+
   return (
     <main className="flex">
-      <AdminSidebar />
+      <AdminSidebar activeLink={"dashboard"} />
       <div className="mr-[1rem] flex h-[100vh] w-full">
         <div className="ml-[1rem] flex h-16 w-full items-center">
-          <h4>Dashboard</h4>
+          <h4>
+            <div className="flex gap-4">
+              <span>Dashboard</span>
+              <span className="font-medium text-gray-700">
+                - {user?.publicMetadata.applicationName ?? ""}
+              </span>
+            </div>
+          </h4>
           <div className="ml-auto flex items-center">
             <SignedIn>
               <UserButton />
