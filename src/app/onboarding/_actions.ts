17d65eacb7fcfc8edb09b8834d2d60c01c1e9b98
Atechchatterjee <atechchatterjeee@gmail.com>
2024-09-10 22:07:30 +0530

    feat: clerk onboarding

--- src/app/onboarding/_actions.ts
+++ src/app/onboarding/_actions.ts
@@ -1,6 +1,32 @@
 "use server";
 
 import { auth, clerkClient } from "@clerk/nextjs/server";
+import { db } from "~/server/db";
+import { organisation } from "~/server/db/schema";
+
+async function clerkUpdateUserForOnboarding(
+  userId: string,
+  formData: FormData,
+) {
+  return await clerkClient().users.updateUser(userId, {
+    publicMetadata: {
+      onboardingComplete: true,
+      applicationName: formData.get("applicationName"),
+      applicationType: formData.get("applicationType"),
+    },
+  });
+}
+
+async function createNewOrganisation(formData: FormData) {
+  const name = formData.get("applicationName");
+  if (typeof name === "string") {
+    return db.insert(organisation).values({ name: name });
+  } else {
+    return new Promise((_, reject) =>
+      reject(new Error("Invalid application name as organisation")),
+    );
+  }
+}
 
 export const completeOnboarding = async (formData: FormData) => {
   const { userId } = auth();
@@ -10,13 +36,7 @@ export const completeOnboarding = async (formData: FormData) => {
   }
 
   try {
-    await clerkClient().users.updateUser(userId, {
-      publicMetadata: {
-        onboardingComplete: true,
-        applicationName: formData.get("applicationName"),
-        applicationType: formData.get("applicationType"),
-      },
-    });
+    await Promise.all([clerkUpdateUserForOnboarding, createNewOrganisation]);
     return { message: "User metadata Updated" };
   } catch (e) {
     console.log("error", e);
